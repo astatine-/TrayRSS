dfd810d084b53bc17c78861267f760562b6e14a1
astatine- <arun - at - tanksali - dot - com>
2012-08-28 18:36:22 +0530

    Framework in place now.
    librssc linked to trayrss. In timer poll of RSS feeds done. Bubble
    notifications completed. Click on bubble leads to launch of browser.
    The grunt work needs to begin ...

---
+++ trayrss.h
@@ -0,0 +1,18 @@
+#ifndef __TRAYRSS_H__
+#define __TRAYRSS_H__
+
+#include <windows.h>
+#include <stdio.h>
+#include <shlwapi.h>
+#include <shellapi.h>
+
+#define TRAYRSS RSS_text("TrayRSS")
+#define TRAYRSSCLASS RSS_text("TrayRSSClass")
+#define URL RSS_text("http://tanksali.com")
+#define VERSION RSS_text("0.1")
+
+#define strAbout RSS_text("TrayRSS version 0.1\n (c) astatine")
+
+#define TRAYRSS_MASTER_RSS_URL RSS_text("http://www.tanksali.com")
+
+#endif
